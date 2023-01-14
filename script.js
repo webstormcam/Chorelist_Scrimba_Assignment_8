5e39b960dde076ac5b7d42b9eef5ebcb778ae568
Cameron Chandler <webstormcam@gmail.com>
2023-01-14 00:08:18 -0500

    Added in comments!
    So developers understand what my functions do

--- script.js
+++ script.js
@@ -5,20 +5,24 @@ const enterChores = document.getElementById('enter')
 const removeChores = document.getElementById('delete')
 let storedChores = JSON.parse(localStorage.getItem("chores"))
 
+
+//Checks if any chores are located in the local storage.
 if(storedChores){
     arrayOfChores = storedChores
     render()
     
 }
 
+
+
+//Takes user input and saves the chore in an array and in local storage for later reference and renders the active chores. 
 enterChores.addEventListener('click',function(){
     for(const chore of arrayOfChores){
         if(chore===taskInput.value){
             return
         }
     }
-    
-    if(taskInput.value!==""){
+     if(taskInput.value!==""){
      arrayOfChores.push(`${taskInput.value}`)
      localStorage.setItem("chores",JSON.stringify(arrayOfChores))
       taskInput.value=""
@@ -26,13 +30,18 @@ enterChores.addEventListener('click',function(){
     }
 })
 
+
+/// This removes all the chores in one click by this function and clears the page.
 removeChores.addEventListener('click',function(){
   localStorage.clear()
   arrayOfChores=[]
   activeChores.innerHTML=''
 })
 
-
+/*
+If you click on an individual chore it will be removed
+by this attached function below.
+*/
 function removeIndividualChore(el){
     let element = el
     for(let i=0;i<arrayOfChores.length;i++){
@@ -44,7 +53,7 @@ function removeIndividualChore(el){
     element.remove() 
 }
 
-
+//This function renders all the tasks on the page.
 function render(){
     let printedChores=""
     for(const chore of arrayOfChores){
